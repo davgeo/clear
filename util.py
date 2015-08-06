e2b72de5dd5903be31346e66e5703a265abb0ccb
David George <dcg.git@gmail.com>
2015-08-06 22:13:42 +0100

    Tidy up commented out print statements in util.py

--- util.py
+++ util.py
@@ -160,13 +160,12 @@ def GetBestStringMatchValue(string1, string2):
   string1 = ''.join(i for i in string1 if i.isalnum())
   string2 = ''.join(i for i in string2 if i.isalnum())
 
-  #print("Finding best match value between {0} and {1}".format(string1, string2))
+  # Finding best match value between string1 and string2
   if len(string1) == 0 or len(string2) == 0:
     bestRatio = 0
   elif len(string1) == len(string2):
     match = difflib.SequenceMatcher(None, string1, string2)
     bestRatio = match.ratio()
-    #print("Match ratio is {0}".format(bestRatio))
   else:
     if len(string1) > len(string2):
       shortString = string2
@@ -177,15 +176,12 @@ def GetBestStringMatchValue(string1, string2):
 
     match = difflib.SequenceMatcher(None, shortString, longString)
     bestRatio = match.ratio()
-    #print("Baseline match ratio is {0}".format(bestRatio))
+
     for block in match.get_matching_blocks():
-      #print(block)
       subString = longString[block[1]:block[1]+block[2]]
       subMatch = difflib.SequenceMatcher(None, shortString, subString)
-      #print("Substring ({0}) matches {1} with ratio {2}".format(subString, shortString, subMatch.ratio()))
       if(subMatch.ratio() > bestRatio):
         bestRatio = subMatch.ratio()
-        #print("Substring ({0}) matches {1} with ratio {2}".format(subString, shortString, subMatch.ratio()))
 
   return(bestRatio)
 
